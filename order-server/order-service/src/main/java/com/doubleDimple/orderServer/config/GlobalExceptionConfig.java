f4ddbac2b4c160b519276a967d3a65f47576714e
doubleDimple <lovele.cn@gmail.com>
2023-05-31 00:05:41 +0800

    增加admin的支持

--- order-server/order-service/src/main/java/com/doubleDimple/orderServer/config/GlobalExceptionConfig.java
+++ order-server/order-service/src/main/java/com/doubleDimple/orderServer/config/GlobalExceptionConfig.java
@@ -2,14 +2,17 @@ package com.doubleDimple.orderServer.config;
 
 import common.exception.exception.CustomException;
 import common.response.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionConfig {
     @ExceptionHandler(CustomException.class)
     public Result<CustomException> handle(CustomException e){
         e.printStackTrace();
+        log.info("出粗了:{{}}",e);
         return Result.fail(e.getCode(),e.getMessage());
     }
 
